b3918459b74b2fa499e276e870332b57d0c95290
Anders Säfsten <69458018+saefsten@users.noreply.github.com>
2021-08-23 09:52:01 +0200

    files created

---
+++ src/main/java/com/mmd/MakeMyDay/MakeMyDayApplication.java
@@ -0,0 +1,13 @@
+package com.mmd.MakeMyDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmd/MakeMyDay/MakeMyDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmd.MakeMyDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMyDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
