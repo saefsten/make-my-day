e02c4c2c3de1e4fe5fc96c395db6d2a88cce9fdf
Marie Svensson <svenssonmc84@gmail.com>
2021-08-23 16:53:34 +0200

    working on login and register

--- src/main/java/com/mmd/MakeMyDay/LoginController.java
+++ src/main/java/com/mmd/MakeMyDay/LoginController.java
@@ -1,7 +1,37 @@
 package com.mmd.MakeMyDay;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "user/login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "user/register";
+    }
+
+   @PostMapping("/register")
+    public String registerNewUser(HttpSession session, @RequestParam String firstName, @RequestParam  String lastName, @RequestParam String email, @RequestParam String password){
+        User user = new User(firstName, lastName, email, password);
+        userRepository.save(user);
+        return "start";
+    }
+
 }
--- src/main/java/com/mmd/MakeMyDay/SecurityConfig.java
+++ src/main/java/com/mmd/MakeMyDay/SecurityConfig.java
@@ -16,13 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/h2", "/h2/**").permitAll()
+                .antMatchers("/", "/h2", "/h2/**", "/register").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/secret", true)
+                .defaultSuccessUrl("/start", true)
                 .permitAll();
     }
 
